cea5dad3887961d28976101fdc249ba48b9f834f
danknjoroge <dank8505@gmail.com>
2022-03-31 22:49:49 +0300

    added navbar

--- stories/urls.py
+++ stories/urls.py
@@ -1,8 +1,14 @@
 from . import views
 from django.urls import path, re_path, include
+from django.contrib.auth import views as auth_views
+from django.contrib.auth.views import LoginView
+
 
 urlpatterns = [
     re_path(r'^$', views.index, name='index'),
     path('accounts/', include('django_registration.backends.one_step.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
+    re_path(r'^logout/$', auth_views.LogoutView.as_view(), name='logout'), 
+    re_path(r'^login/$', LoginView.as_view(), {"next_page": '/'}),
+
 ]
