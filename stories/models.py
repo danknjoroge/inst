73955dfc74134cb2f2edf968043d1bfdf22b7b62
danknjoroge <dank8505@gmail.com>
2022-03-31 08:55:48 +0300

    added image model

--- stories/models.py
+++ stories/models.py
@@ -1,3 +1,16 @@
+from email.mime import image
 from django.db import models
 
 # Create your models here.
+class Image(models.Model):
+    image = models.ImageField()
+    img_name= models.CharField(max_length=44)
+    img_caption= models.CharField(max_length=277)
+    likes = models.IntegerField()
+    comments = models.CharField(max_length=1001)
+
+    def save_image(self):
+        self.save()
+
+    def delete_image(self):
+        self.delete()
