4222b44a414dbf2243819be6880e8a4829006f9b
danknjoroge <dank8505@gmail.com>
2022-04-04 20:37:00 +0300

    deployment

--- stories/admin.py
+++ stories/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models  import Image, Profile
+from .models  import Comments, Image, Profile
 
 
 # Register your models here.
 admin.site.register(Image)
 admin.site.register(Profile)
+admin.site.register(Comments)
